0de585274d0a25f469c9cf433571d357f5c05ea5
AdrianCC9 <adrian.caricari9@gmail.com>
2024-11-28 18:34:34 -0500

    created validation script to ensure annotations match images

--- test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-artificial_annotations_file = "/Users/adrian/Documents/Tell2Design Data/General Data/Tell2Design_artificial_all.pkl"
-
-with open(artificial_annotations_file, "rb") as f:
-    artificial_annotations = pickle.load(f)
-
-print(type(artificial_annotations))  # Check the type of data (e.g., list, dict, etc.)
-print(artificial_annotations[0])  # Print the first element to examine the structure
